654e70694202567ba360cc1e21f3d1fccf6df2f5
bvshreedevi <shreedevi.bv11@gmail.com>
2021-04-04 15:56:06 +0530

    first deploy

--- src/app/autherization/register/register.component.ts
+++ src/app/autherization/register/register.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
-
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
